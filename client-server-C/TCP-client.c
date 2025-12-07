6c95534153c328ef2905f07e002e478347c5afb3
SteliosSpanos <stylspan19@gmail.com>
2025-12-07 16:20:56 +0200

    Adds error handling

--- client-server-C/TCP-client.c
+++ client-server-C/TCP-client.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main() {
-
-	int net_socket = socket(AF_INET, SOCK_STREAM, 0);
+	
+	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket == -1) {
+		printf("Socket failed\n");
+		exit(1);
+	}
 
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(12000);
-	server_address.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
+	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
 						     
-	int status = connect(net_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+	int status = connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address));
 	if (status == -1) {
 		printf("COnnection failed\n");
 		exit(1);
 	}
 
+	char message[256];
+	printf("Enter a message: ");
+	fgets(message, sizeof(message), stdin);
+	int send_status = send(client_socket, message, strlen(message), 0);
+	if (send_status == -1) {
+		printf("Sending message failed\n");
+		exit(1);
+	}
+
 	char response[256];
-	recv(net_socket, &response, sizeof(response), 0);
+	int recv_status = recv(client_socket, response, sizeof(response), 0);
+	if (recv_status == -1) {
+		printf("Receiving message failed\n");
+		exit(1);
+	}
+	response[recv_status] = '\0';
 
 	printf("From Server: %s\n", response);
 
-	close(net_socket);
+	close(client_socket);
 
 	return 0;
 }
