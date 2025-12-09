ab52b19240299d648e51800dec67ff87e6e8bfc5
SteliosSpanos <stylspan19@gmail.com>
2025-12-10 00:06:18 +0200

    Fixes typo

--- http-server/http-client.c
+++ http-server/http-client.c
@@ -16,4 +16,37 @@ int main() {
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(8080);
-	server_address.sin_addr.s_adddr = inet_addr("127.0.0.1");
+	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	int connect_status = connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+	if (connect_status == -1) {
+		perror("connect:");
+		close(client_socket);
+		exit(1);
+	}
+
+	const char *request =
+          	"GET / HTTP/1.1\r\n"
+          	"Host: localhost\r\n"
+          	"Connection: close\r\n"
+          	"\r\n";
+
+	int send_status = send(client_socket, request, strlen(request), 0);
+	if (send_status == -1) {
+		perror("send:");
+		exit(1);
+	}
+
+	char buffer[4096];
+	ssize_t bytes;
+
+	printf("Response:\n");
+	while ((bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0)) > 0) {
+		buffer[bytes] = '\0';
+		printf("%s\n", buffer);
+	}
+
+	close(client_socket);
+	return 0;
+}
+	
