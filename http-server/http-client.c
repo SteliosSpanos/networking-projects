f588913466bc8f58c5c1fd8e04181a802b199252
SteliosSpanos <stylspan19@gmail.com>
2025-12-10 09:58:00 +0200

    Removes ':' from all perror()

--- http-server/http-client.c
+++ http-server/http-client.c
@@ -9,7 +9,7 @@
 int main() {
 	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (client_socket == -1) {
-		perror("socket:");
+		perror("socket");
 		exit(1);
 	}
 
@@ -20,20 +20,21 @@ int main() {
 
 	int connect_status = connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address));
 	if (connect_status == -1) {
-		perror("connect:");
+		perror("connect");
 		close(client_socket);
 		exit(1);
 	}
 
 	const char *request =
-          	"GET / HTTP/1.1\r\n"
-          	"Host: localhost\r\n"
-          	"Connection: close\r\n"
-          	"\r\n";
+		"GET / HTTP/1.1\r\n"
+		"Host: localhost\r\n"
+		"Connection: close\r\n"
+		"\r\n";
 
 	int send_status = send(client_socket, request, strlen(request), 0);
 	if (send_status == -1) {
-		perror("send:");
+		perror("send");
+		close(client_socket);
 		exit(1);
 	}
 
@@ -43,10 +44,9 @@ int main() {
 	printf("Response:\n");
 	while ((bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0)) > 0) {
 		buffer[bytes] = '\0';
-		printf("%s\n", buffer);
+		printf("%s", buffer);
 	}
 
 	close(client_socket);
 	return 0;
 }
-	
